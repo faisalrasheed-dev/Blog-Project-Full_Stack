89ec4b7ef3ba56b2365b63f8b0fba45af514387d
Muhammed Faisal R <faisalrasheed.pro@gmail.com>
2025-09-16 13:11:32 -0930

    Updated MainLayout to make background full screen + adjusted article content padding

--- front-end/src/components/MainLayout.jsx
+++ front-end/src/components/MainLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Mainlayout = ({children}) => {
   return (
-    <main className="p-6 md:p-20 font-sans bg-gray-100 h-full">
+    <main className=" w-full min-h-screen p-6 md:p-20 font-sans bg-gray-100 h-full">
         {children}
     </main>
   )
